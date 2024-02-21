dab856f06aba19cf08e7c02da17627640a25a5e8
primaryyuvi <yuvarajssgss4@gmail.com>
2024-02-21 16:14:19 +0530

    almost finished with article and my profile components

--- src/app/menu/menu-article/services/menu-article.service.ts
+++ src/app/menu/menu-article/services/menu-article.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MenuArticleService {
+  constructor(private _http: HttpClient) {}
 
-  constructor() { }
+  newsApiUrl =
+    'https://newsapi.org/v2/everything?q=food&apiKey=db20ce91c53541ec87f8e282274bad7a';
+
+  getArticles() { 
+    return this._http.get(this.newsApiUrl);
+  }
 }
